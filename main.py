509f7f09e25559aafa4ca9b4b94007aa4124c566
yl826 <yl826@duke.edu>
2022-11-23 01:34:19 -0500

    Add support to change volume in range of [0-100]

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, json, app
+import json
+
+from flask import Flask, request, jsonify
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -6,46 +8,58 @@ from comtypes import CoInitialize, CoUninitialize
 
 from keyboard import Keyboard
 
-companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
-
 api = Flask(__name__)
 
 
-@api.route('/mute', methods=['GET'])
+@api.route('/mute', methods=['POST'])
 def set_mute():
-    muted = ""
-    CoInitialize()
-    Keyboard.key(Keyboard.VK_VOLUME_MUTE)
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(
-        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    if request.get_data().decode('UTF-8') == "true":
+        set_status(True, None)
+    else:
+        set_status(False, None)
+    return get_status()
 
-    #volume.SetMasterVolumeLevel(-20.0, None)
-    volume.GetVolumeRange()
-    muted = volume.GetMute()
-    print(muted)
-    CoUninitialize()
 
-    # volume.SetMute()
-    return "Muted"if muted == 1else "Unmuted"
+@api.route('/volume', methods=['Post'])
+def set_volume():
+    volume_level = int(request.get_data().decode('UTF-8'))
+    if volume_level > 100 or volume_level < 0:
+        return "Illegal volumeLevel", 400
+    set_status(None, volume_level)
+    return get_status()
 
-@api.route('/mute_state', methods=['GET'])
-def get_mute():
-    muted = ""
+
+@api.route('/status', methods=['GET'])
+def get_status():
     CoInitialize()
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(
         IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-    volume.GetVolumeRange()
-    muted = volume.GetMute()
-    print(muted)
+    responseJson = {
+        "Muted": [ "1" if volume.GetMute()==0else "0"],
+        "VolumeLevel": [round(volume.GetMasterVolumeLevelScalar() * 100)]
+    }
     CoUninitialize()
-    # volume.SetMute()
-    return "Muted"if muted == 1else "Unmuted"
+    return responseJson
+
+
+def set_status(mute=None, volume_level=None):
+    status = get_status()
+    print(mute)
+    print(status["Muted"][0])
+    if (mute is True and status["Muted"][0] == "0") or (mute is False and status["Muted"][0]  == "1" ):
+        Keyboard.key(Keyboard.VK_VOLUME_MUTE)
+    if volume_level is not None:
+        CoInitialize()
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(
+            IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        volume.SetMasterVolumeLevelScalar(round(volume_level / 100, 2), None)
+        CoUninitialize()
 
 
 if __name__ == '__main__':
-    api.run(host = '0.0.0.0')
+    api.run(host='0.0.0.0')
